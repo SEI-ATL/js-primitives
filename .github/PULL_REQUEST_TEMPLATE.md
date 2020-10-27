a5c7ed4175fda41e1b066c4d751938f2f91a2190
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2020-10-26 20:02:06 -0700

    [js] Complete exercises as well as attempt at bonus but incomplete

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -2,8 +2,9 @@
 
 Please describe your comfort and completeness levels before submitting.
 
-Comfort Level (1-5): 
+Comfort Level (1-5): 4
 
-Completeness Level (1-5):
+Completeness Level (1-5): 5
 
 What did you think of this deliverable?:
+It was fun to do some of these exercises and see how much information I have retained. Especially when at times I feel like I don't know what is going on. So its nice to know there is progress.
--- app.js
+++ app.js
@@ -112,40 +112,46 @@ console.log(ourFriends);
 // 6.) I have a list of favorite foods below. If `Popcorn` is my favorite food and `Potato chips` my second favorite, then how would you find the rank of another food? Try finding the rank of `Pho`.
 
 // ```js
-// const foods = [
-//   'Popcorn',
-//   'Potato chips',
-//   'Shrimp',
-//   'Chicken rice',
-//   'Poutine',
-//   'Tacos',
-//   'Toast',
-//   'French Toast',
-//   'Crab',
-//   'Pho',
-//   'Lasagna',
-//   'Brownie',
-//   'Lobster',
-//   'Donuts',
-//   'Ice cream',
-//   'Hamburger',
-//   'Sushi',
-//   'Chocolate',
-//   'Pizza'
-// ];
+const foods = [
+  'Popcorn',
+  'Potato chips',
+  'Shrimp',
+  'Chicken rice',
+  'Poutine',
+  'Tacos',
+  'Toast',
+  'French Toast',
+  'Crab',
+  'Pho',
+  'Lasagna',
+  'Brownie',
+  'Lobster',
+  'Donuts',
+  'Ice cream',
+  'Hamburger',
+  'Sushi',
+  'Chocolate',
+  'Pizza'
+];
 // ```
 
 
-function rankedFoods(foods) {
-    for (let i = 0; i < foods.length; i++) {
-
-    }
-}
-
-
+let phoRank = (foods.indexOf('Pho') + 1);
+console.log(phoRank);
+console.log(foods[9] + " is number " + phoRank + " in the rankings.");
 // 7.) I made a mistake with my favorite foods. How can I find the index of `Donuts` and remove it? **(Hint: look up `splice`)**
 
+
+foods.splice(foods.indexOf('Donuts'));
+console.log(foods);
+
+
 // 8.) My friends want to know what my `5`th to `10`th favorite foods are. How would I find these foods?
+
+
+let fiveToTen = foods.slice(4, 10);
+console.log(fiveToTen);
+
 
 // 9.) Change the word "Space" to the word "Spice".
 
@@ -156,7 +162,10 @@ word = "Spice";
 
 console.log(word);
 
+
+
 // 10.) **BONUS** - Here are a list of `ages`
+
 
 const ages = [83, 53, 37, 29, 60, 30, 66, 19, 59, 41, 9, 64, 19, 80, 24, 53, 70, 1, 53, 40, 92, 4, 71, 65, 8, 2, 51, 80, 94, 37, 80, 64, 19, 6, 14];
 
@@ -164,7 +173,7 @@ const ages = [83, 53, 37, 29, 60, 30, 66, 19, 59, 41, 9, 64, 19, 80, 24, 53, 70,
 
 function median = medianNum => {
     const middle = math.floor(medianNum.length / 2)
-    
+
 }
 
 const
