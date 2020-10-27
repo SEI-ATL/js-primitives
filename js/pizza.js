39aad4fb026f1b10bf1d8b35149d39cf9e7cf431
MusicMan <musicman@MacBook-Pro.fios-router.home>
2020-10-27 00:51:48 -0400

    [primitives] add js array practice

---
+++ js/pizza.js
@@ -0,0 +1,34 @@
+// 1 Friends array
+console.log(friends[friends.length-1]);
+console.log(friends.pop());
+
+// 2 alphabetical sort
+console.log(friends.sort());
+ var alphabeticalarray = "Moe,larry,curly,Jane,Emma,Elizabeth,Elinor,Mary,Darcy,Grey,Lydia,Harriet";
+
+//  3 will definitely need a better look into.
+var friendsString = 'Moe,Larry,Curly,Jane,Emma,Elizabeth,Elinor,Mary,Darcy,Grey,Lydia,Harriet';
+ let newNames = friendsString.split(",");
+ console.log(newNames.sort());
+
+// 4 reverse alphabetical order
+console.log(newNames.reverse());
+
+// 5
+
+
+// 6 
+console.log(foods.indexOf(Pho));
+
+// 7 looking up an index and removing it. 
+foods.splice(foods.indexOf('Donuts'), 1);
+console.log(foods);
+
+
+//8 0 based index favorite foods
+console.log(food.slice(4,9));
+
+// 9 change the word space to spice.(This threw me off) 
+food = space;
+let food === 'spice';
+
