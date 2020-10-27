c8b64e0685e331ac569a9b095c69117636e47fe2
Amari James SEI <73178321+marjames98@users.noreply.github.com>
2020-10-27 09:19:58 -0400

    Create pizza.js

---
+++ js/pizza.js
@@ -0,0 +1,72 @@
+const friends = [
+  'Moe',
+  'Larry',
+  'Curly',
+  'Jane',
+  'Emma',
+  'Elizabeth',
+  'Elinor',
+  'Mary',
+  'Darcy',
+  'Grey',
+  'Lydia',
+  'Harriet'
+];
+console.log(friends[friends.length-1])
+
+var friends = 'Moe,Larry,Curly,Jane,Emma,Elizabeth,Elinor,Mary,Darcy,Grey,Lydia,Harriet';
+friends.sort();
+
+const myFriends = [
+  'Rickon',
+  'Meera',
+  'Hodor',
+  'Jojen',
+  'Osha',
+  'Rickard',
+  'Maester',
+  'Rodrik',
+  'Jory',
+  'Septa',
+  'Jon'
+];
+
+const yourFriends = [
+  'Bilbo',
+  'Boromir',
+  'Elrond',
+  'Faramir',
+  'Frodo',
+  'Gandalf',
+  'Legolas',
+  'Pippin'
+];
+mergeTwo(myFriends, yourFriends);
+Arrays.sort(arr)
+
+const foods = [
+  'Popcorn',
+  'Potato chips',
+  'Shrimp',
+  'Chicken rice',
+  'Poutine',
+  'Tacos',
+  'Toast',
+  'French Toast',
+  'Crab',
+  'Pho',
+  'Lasagna',
+  'Brownie',
+  'Lobster',
+  'Donuts',
+  'Ice cream',
+  'Hamburger',
+  'Sushi',
+  'Chocolate',
+  'Pizza'
+];
+
+arr.splice(Donuts);
+
+let word = "Space";
+replace("Space, Spice")
